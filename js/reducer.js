c361034c3c29517ad4fe74799f578c64697c50da
sarabastian <seb118@georgetown.edu>
2021-02-06 12:52:48 -0500

    done

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
 // add code snippets from README
+let state 
+
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+} 
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }  
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+  } 
+
+  dispatch({type: '@@INIT'}) 
+
+//   dispatch({type: 'INCREASE_COUNT'})
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+    dispatch({type: 'INCREASE_COUNT'})
+  }) 
+
+
